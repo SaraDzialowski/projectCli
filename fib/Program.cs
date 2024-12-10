bcc2cd30b90c2c7f4f60ccaf8c95241ea7b5989c
SaraDzialowski <msd598642@gmail.com>
2024-12-10 18:57:05 +0200

    upload the project

---
+++ fib/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CommandLine;
+var bundleOption = new Option<FileInfo>("--output", "File path and name");
+
+var bundleCommand = new Command("bundle", "bundle code files to a single file");
+bundleCommand.AddOption(bundleOption);
+bundleCommand.SetHandler((FileInfo output) =>
+{
+    try
+    {
+        using (File.Create(output.FullName)) { }
+        Console.WriteLine("file was created");
+    }
+    catch (DirectoryNotFoundException ex)
+    {
+        Console.WriteLine("file path is invalid");
+    }
+}, bundleOption);
+
+var rootCommand = new RootCommand("Root command for File bundler CLI");
+rootCommand.AddCommand(bundleCommand);
+await rootCommand.InvokeAsync(args);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
